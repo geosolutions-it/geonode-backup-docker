7143253c22b955ffaf18d992257a34627b7d1873
Cezary Statkiewicz <cezio@thelirium.net>
2018-04-18 18:47:18 +0200

    initial

---
+++ fs_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo 'run fs backup', date
+env
+# /$deployment/pg
+_TARGET_DIR=/${RANCHER_STACK:-geonode-generic-${HOSTNAME}}/fs/
+
+# /mnt/volumes/backup/$deployment/pg/$date
+TARGET_DIR=${TARGET_DIR:-/mnt/volumes/backups/}/${TARGET_DIR}/$(date +%Y%m%d)
+
+# all above + filename
+TARGET_FILE=${TARGET_DIR}/data-$(date '+%Y%m%d').tar.gz
+
+mkdir -p ${TARGET_DIR}
+
+rclone --config /root/rclone.config copy local:/mnt/volumes/data/  dest:${TARGET_DIR}/data/
+rclone --config /root/rclone.config copy local:/mnt/volumes/statics/  dest:${TARGET_DIR}/statics/
+
+
---
+++ pg_backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## backup script for postgres db
+# define env vars to be used by pg client tools 
+# PGUSER
+# PGHOST
+# PGPORT
+# PGDATABASE
+# PGOPTIONS
+
+echo 'run pg backup', date
+env
+# get hostname 
+HOSTNAME=${HOSTNAME:-$(hostname)}
+
+# /$deployment/pg
+_TARGET_DIR=/${RANCHER_STACK:-geonode-generic-${HOSTNAME}}/pg/
+
+# /mnt/volumes/backup/$deployment/pg/$date
+TARGET_DIR=${TARGET_DIR:-/mnt/volumes/backup/}/${TARGET_DIR}/$(date +%Y%m%d)
+
+# all above + filename
+TARGET_FILE=${TARGET_DIR}/pg_dumpall-$(date '+%Y%m%d').tar.gz
+
+mkdir -p ${TARGET_DIR}
+
+
+# we don't provide credentials nor db location, since it's comming from env
+pg_dumpall > ${TARGET_FILE}
